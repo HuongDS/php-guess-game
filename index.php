dce6a61bb9af33cd6f6235cb1a8c1381963d3c73
HuongDS <146117043+HuongDS@users.noreply.github.com>
2025-05-20 00:05:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,23 +7,30 @@
   <h1>Welcome to my guessing game</h1>
 
   <?php
-  $correctnumber = 26;
-
-  if (isset($_GET['guess'])) {
-      $guess = $_GET['guess'];
-      if (!is_numeric($guess)) {
-          echo "Your guess is not a number";
-      } else if ($guess < $correctnumber) {
-          echo "Your guess is too low";
-      } else if ($guess > $correctnumber) {
-          echo "Your guess is too high";
-      } else {
-          echo "Congratulations - You are right";
-      }
-  } else {
-      echo "Missing guess parameter <br />";
-      echo "Your guess is too short";
-  }
-  ?>
+$correctnumber=26; //change this number //
+if(isset($_GET['guess']))
+{
+  if (is_numeric($_GET['guess'])===FALSE)
+ {
+echo "Your guess is not a number";
+}
+else if ($_GET['guess']<$correctnumber)
+ {
+echo "Your guess is too low";
+}
+else if ($_GET['guess']>$correctnumber)
+ {
+echo "Your guess is too high";
+}
+else if ($_GET['guess']==$correctnumber)
+ {
+echo "Congratulations - You are right";
+}
+}
+else
+{
+echo "Missing guess parameter <br> Your guess is too short";
+}
+?>
 </body>
 </html>
