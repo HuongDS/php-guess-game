a0f26e5a081d416d0b1fd6f0ad917e64b4bed944
HuongDS <146117043+HuongDS@users.noreply.github.com>
2025-06-09 10:51:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <h1>Welcome to my guessing game</h1>
 
   <?php
-$correctnumber=45; //change this number //
+$correctnumber=50; //change this number //
 if(isset($_GET['guess']))
 {
   if (is_numeric($_GET['guess'])===FALSE)
